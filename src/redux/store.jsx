45db3ca2c14c2c26ed7c6aa9dcb7c25a9d1cd80d
Oleh Tsap <oleh.craftman@gmail.com>
2024-01-25 20:16:59 +0200

    рефактор коду

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,20 +1,20 @@
-import { legacy_createStore as createStore } from 'redux';
-import { devToolsEnhancer } from "@redux-devtools/extension";
-import { statusFilters } from './constants';
+import { taskReducer } from "./taskSlice";
+import { filterReducer } from "./filterSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { combineReducers, legacy_createStore as createStore } from "redux";
 
-const initialState = {
-  tasks: [
-    { id: 0, text: "Learn HTML and CSS", completed: true },
-    { id: 1, text: "Get good at JavaScript", completed: true },
-    { id: 2, text: "Master React", completed: false },
-    { id: 3, text: "Discover Redux", completed: false },
-    { id: 4, text: "Build amazing apps", completed: false },
-  ],
-  filters: {
-    status: statusFilters.all,
-  },
-};
+const persistConfig = {
+  key: 'root',
+  storage
+}
 
-const rootReducer = (state = initialState, action) => state;
-const enhancer = devToolsEnhancer();
-export const store = createStore(rootReducer, enhancer);
+const rootReducer = combineReducers({
+  tasks: taskReducer,
+  filters: filterReducer,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = createStore(persistedReducer)
+export const persistor = persistStore(store)
